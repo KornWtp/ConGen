7565c789c8c070d58c9a3939ddd8fb8e577a8b17
Mr.P L <pingloaf@gmail.com>
2022-03-13 17:05:10 +0700

    Update train_congen.sh

--- train_congen.sh
+++ train_congen.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+# please see Appendix A.1 for the full setup
 CUDA_VISIBLE_DEVICES=$1
 python main.py \
     --model_save_path "your-output-model-path" \
-    --teacher_model_name_or_path princeton-nlp/unsup-simcse-roberta-large \
-    --student_model_name_or_path nreimers/BERT-Tiny_L-2_H-128_A-2 \
-    --train_data_path "your-train-data-path" \
-    --dev_data_path "your-validation-data-path" \
+    --teacher_model_name_or_path princeton-nlp/unsup-simcse-roberta-large \ # Default teacher
+    --student_model_name_or_path nreimers/BERT-Tiny_L-2_H-128_A-2 \ # compressed model or large model
+    --train_data_path "your-train-data-path" \ # attached link
+    --dev_data_path "your-validation-data-path" \ # STS-B dev set
     --train_batch_size 128 \
     --eval_batch_size 128 \
     --max_seq_length 128 \
     --num_epochs 20 \
-    --learning_rate 5e-5 \
-    --teacher_temp 0.1 \
-    --student_temp 0.09 \
-    --queue_size 16384
+    --learning_rate 5e-4 \ # see Appendix A.1
+    --teacher_temp 0.05 \ # see Appendix A.1
+    --student_temp 0.05 \ # see Appendix A.1
+    --queue_size 16384 # see Appendix A.1
