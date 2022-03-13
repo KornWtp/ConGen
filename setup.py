838f6731a569965e6218835796d28bef6a9bbf31
KornWtp <elecwpr@gmail.com>
2022-03-13 16:45:11 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", mode="r", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+
+
+setup(
+    name="congen-sbert",
+    version="1.0.0",
+    author=" ",
+    author_email=" ",
+    description="Sentence representation with SBERT",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KornWtp/ConGen",
+    download_url="https://github.com/KornWtp/ConGen.git",
+    packages=find_packages(),
+    install_requires=[
+        "torch==1.8.1",
+        "transformers==4.9.0",
+        "sentence-transformers==2.0.0",
+        "pandas==1.4.1",
+    ],
+)
